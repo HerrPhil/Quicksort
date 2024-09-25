a5053dad147888765bef4d24f653765e2e71ddbd
HerrPhil <phillip.j.wray.esq@gmail.com>
2024-09-25 15:30:55 -0600

    Write the partition() method with a lot of logging

--- Quicksort.java
+++ Quicksort.java
@@ -14,6 +14,7 @@ public class Quicksort {
 
         // sample test
         int [] sample = new int [] {10, 80, 30, 90, 40};
+        // int [] sample = new int [] {10, 80, 30, 90, 40, 50, 70};
 
         Quicksort quicksort = new Quicksort(sample);
 
@@ -35,8 +36,6 @@ public class Quicksort {
     }
 
     public int [] solution() {
-
-        // TODO - more
 
         int N = this.original.length;
 
@@ -63,34 +62,80 @@ public class Quicksort {
 
         // Choosing the pivot value - pick high
         int pivot = original[high];
+
+        System.out.printf("The pivot value is %d%n", pivot);
         
         // Index of smaller element and indicates the
         // right (future) position of pivot found so far
 
         int smallerElement = (low - 1);
 
-        // Notice the very first i value when low is 0;
-        // it is -1. Then it increments to 0 prior to swap()
-        System.out.printf("partition() initialize smaller element location %d%n", smallerElement);
+        // Notice the very first smaller element index is -1 when low is 0.
+        // Then it increments to 0 prior to swap()
+        System.out.printf("partition() initialize smaller element index %d%n", smallerElement);
 
         int highBeforePivot = high - 1;
 
         System.out.printf("partition() the index of high before the pivot (high) %d%n", highBeforePivot);
 
+        // Iterate over all values before high pivot value
         for (int currentElement = low; currentElement <= highBeforePivot; currentElement++) {
 
-            System.out.printf("partition() for every current element index %d%n", currentElement);
+            System.out.printf("partition() for the current element index %d%n", currentElement);
 
             // A note here about what we are not doing.
-            // When the current element value is greater than or equal to pivot value,
-            // then skip the swap.
+            // When the current element value is greater than or equal to the pivot value,
+            // then skip incrementing the smaller element index and skip the swap.
             // Leave the same or bigger behind.
             // This sets it up for the next swap.
+            if (original[currentElement] >= pivot) {
+                System.out.printf("partition() the current element value %d is greater than or equal to the pivot value%n", original[currentElement]);
+                System.out.printf("partition() retain smaller element index %d%n", smallerElement);
+                System.out.printf("Skip swap%n");
+            }
 
-            // TODO - the real stuff
+            // When the current element value is less than the pivot value,
+            // then do the swap.
+            // It is hard to see, but this covers the edge case there smaller element index + 1 = 0
+            // and currentElement = 0. It does a swap in place.
+            // Based on the skip logic above, we know the skipped values are greater than or equal to pivot.
+            // Therefore they can be swapped with the smaller value at the current element index.
+            if (original[currentElement] < pivot) {
+
+                System.out.printf("partition() the current element value %d is greater than or equal to the pivot value%n", original[currentElement]);
+
+                // Increment the index of the smaller element
+                smallerElement++;
+                System.out.printf("partition() incremented smaller element index, its value is now %d%n", smallerElement);
+
+                System.out.printf("Do swap%n");
+                swap(smallerElement, currentElement);
+            }
+
+            System.out.printf("Current state of array is %s%n", Arrays.toString(original));
+
         }
 
-        return -1;
+        // Make the final move.
+        // The logic above guarantees that the value at smaller element index + 1
+        // is greater than or equal to the pivot.
+        // This is the new location of the pivot.
+        // This also puts the bigger value from the left-side partition in the right-side partition with other bigger values.
+        System.out.printf("Finally swap values at positions %d and %d to put pivot value in final position%n", smallerElement + 1, high);
+        swap(smallerElement + 1, high);
+
+        System.out.printf("Final state of array is %s%n", Arrays.toString(original));
+
+        return (smallerElement + 1);
+    }
+
+    private void swap(int smallerElement, int currentElement) {
+        // Store the bigger value at smaller element index
+        int temp = original[smallerElement];
+        // Move the smaller value that is less than pivot to the smaller element index
+        original[smallerElement] = original[currentElement];
+        // Move the bigger value to the current element index
+        original[currentElement] = temp;
     }
 
 }
